c7a1b5841bec1202a0350a7b2823d131e22cbcfb
octowild <139123949+octowild@users.noreply.github.com>
2026-08-19 11:03:43 +0530

    video

---
+++ Pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../Components/Layout';
+
+const Home = () => {
+  return (
+    <Layout>
+        
+        
+
+    </Layout>
+  );
+};
+
+export default Home
